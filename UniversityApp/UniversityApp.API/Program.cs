6b4a6761705619c9dde7e364351a34a607877641
Ahmed Muftić <ahmed.muftic98@gmail.com>
2022-11-15 13:44:25 +0100

    add swager config

--- UniversityApp/UniversityApp.API/Program.cs
+++ UniversityApp/UniversityApp.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using UniversityApp.Application.Mapper;
 using UniversityApp.Infrastructure.DependencyInjection;
 
@@ -11,36 +12,35 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "UniversityApp", Version = "v1" });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "UniversityApp", Version = "v1" });
 
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-//    {
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer",
-//        BearerFormat = "JWT",
-//        In = ParameterLocation.Header,
-//        Description = "JWT Authorization header using the Bearer scheme."
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
 
-//    });
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//                {
-//                    {
-//                          new OpenApiSecurityScheme
-//                          {
-//                              Reference = new OpenApiReference
-//                              {
-//                                  Type = ReferenceType.SecurityScheme,
-//                                  Id = "Bearer"
-//                              }
-//                          },
-//                         new string[] {}
-//                    }
-//                });
-//});
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(ApplicationMapper));
 builder.Services.ConfigureOAuth(builder.Configuration);
